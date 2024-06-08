2b0ffea4b64ee096e942ac5f27e625a908820cf3
Vigneshwaran <125852792+vigneshwaranbalamurugan@users.noreply.github.com>
2024-06-08 14:08:44 +0530

    'homepage'

--- banking_system/main/urls.py
+++ banking_system/main/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/add',views.add,name='add'),  # Root URL
+    path('add',views.home,name='add'),  # Root URL
 ]
